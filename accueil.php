ee57c89694dd34526c941ee3b206b0aa12a09571
101197 <valat.estelle@gmail.com>
2018-05-31 09:46:31 +0200

    connexion a la bdd marche

--- accueil.php
+++ accueil.php
@@ -7,8 +7,6 @@
   </head>
   <body>
     <?php include 'assets/php/nav.php'; ?>
-<?php $reponse = $bdd->query('SELECT * FROM client');
-echo $reponse;?>
 
     <div class="container-fluid">
       <div class="bandeprincipale row mh-100vh">

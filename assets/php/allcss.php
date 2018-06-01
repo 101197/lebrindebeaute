f929b1e969d51781c58b4374cac4767408cd5fd8
101197 <valat.estelle@gmail.com>
2018-06-01 10:22:56 +0200

    rectification background

--- assets/php/allcss.php
+++ assets/php/allcss.php
@@ -1,8 +1,8 @@
 <link rel="stylesheet" href="assets/css/style-inscription.css">
 <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+<link rel="stylesheet" href="assets/css/-Login-form-Page-BS4-.css">
 <link rel="stylesheet" href="assets/css/Bold-BS4-Footer-Big-Logo.css">
 <link rel="stylesheet" href="assets/css/Dark-NavBar.css">
-<link rel="stylesheet" href="assets/css/-Login-form-Page-BS4-.css">
 <link rel="stylesheet" href="assets/css/styles.css">
 <link rel="stylesheet" href="assets/css/style2.css">

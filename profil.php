36fdd855470c4b2bada4e2b722dfa7f29efa49c5
101197 <valat.estelle@gmail.com>
2018-06-07 17:15:42 +0200

    profil

--- profil.php
+++ profil.php
@@ -10,21 +10,29 @@
 
     <div class="container-fluid">
       <div class="bandeprincipale row mh-100vh">
-        <div class="col-10 col-sm-8 col-md-6 col-lg-6 offset-1 offset-sm-2 offset-md-3 offset-lg-0 align-self-center d-lg-flex align-items-lg-center align-self-lg-stretch bg-white p-5 rounded rounded-lg-0 my-5 my-lg-0" id="login-block">
+        <div class="col-10 col-sm-8 col-md-6 col-lg-6 offset-1 offset-sm-2 offset-md-3 offset-lg-0 d-lg-flex bg-white p-5 rounded rounded-lg-0 my-5 my-lg-0" id="login-block">
 
           <div class="designprofil">
+            <?php
+            $idclient = $_SESSION['id'];
+            $requser = $bdd->prepare("SELECT * FROM client WHERE IDClient = ?");
+            $requser->execute(array($idclient));
+            $dbrep = $requser->fetch();
+            ?>
 
-          <img src="assets/img/profilneutre.png" alt="profil" class="imgprofilneutre">
+            <img src="assets/img/profilneutre.png" alt="profil" class="imgprofilneutre">
 
-          <h1 class="pseudoprofil"><?php echo($_SESSION['pseudo']); ?><!-- pseudo --></h1>
+            <h1 class="pseudoprofil"><?php echo $dbrep['Pseudo']; ?></h1>
             <ul class="infoprofil">
-              <li>Nom: <?php echo($_SESSION['nom']); ?><!-- nom --></li>
-              <li>Prénom: <!-- prenom --></li>
-              <li>E-mail: <!-- mail --></li>
-              <li>Téléphone: <!-- telephone --></li>
+              <li><b>Nom: </b><?php echo $dbrep['Nom']; ?></li><br>
+              <li><b>Prénom: </b><?php echo $dbrep['Prenom']; ?></li><br>
+              <li><b>E-mail: </b><?php echo $dbrep['Mail']; ?></li><br>
+              <li><b>Téléphone: </b><?php echo $dbrep['Telephone']; ?></li>
             </ul>
-
+            <button type="button" name="button" class="btn btn-info mt-2" onclick="window.location.href='modifierclient'"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>&nbsp;Modifier</button>
           </div>
+
+
 
         </div>
       </div>

3374d8a59d6f4f04172fe5ff29fc10efe8393e64
101197 <valat.estelle@gmail.com>
2018-06-11 17:04:27 +0200

    footer

--- assets/php/footer.php
+++ assets/php/footer.php
@@ -1,54 +1,16 @@
-    <footer id="myFooter">
-        <div class="container-fluid">
-            <div class="row text-center">
-                <div class="col-12 col-sm-6 col-md-3">
-                    <!-- <h1 class="logo" style="margin-top:30px;"><a href="#">&nbsp;LOGO&nbsp;</a></h1> -->
-                    <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11732.03133264411!2d2.7934539!3d42.6823764!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe3fa136135e70af9!2sInstitut+Le+Brin+de+Beaut%C3%A9!5e0!3m2!1sfr!2sfr!4v1526977004589" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+<footer id="myFooter">
+  <div class="ensemblefooter">
 
-                </div>
-                <div class="textfooter row text-center">
-                <!-- <div class="col-12 col-sm-6 col-md-2">
-                    <h5>Get started</h5>
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Downloads<br></a></li>
-                        <li><a href="#">Sign Up</a></li>
-                        <li><a href="#">Other Links</a></li>
-                    </ul>
-                </div>
-                <div class="col-12 col-sm-6 col-md-2">
-                    <h5>Our Company</h5>
-                    <ul>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Company Information<br></a></li>
-                        <li><a href="#">Reviews</a></li>
-                        <li><a href="#">Contacts</a></li>
-                    </ul>
-                </div>
-                <div class="col-sm-6 col-md-2">
-                    <h5>Support</h5>
-                    <ul>
-                        <li><a href="#">FAQ</a></li>
-                        <li><a href="#">Help Desk<br></a></li>
-                        <li><a href="#">Forums</a></li>
-                        <li><a href="#">External Links</a></li>
-                    </ul>
-                </div> -->
-                <div class="col-md-3 social-networks">
-                    <ul>
-                      <li><a class="adresse"><i class="fa fa-map-marker">&nbsp;Place&nbsp;de&nbsp;la&nbsp;Rébuplique&nbsp;-&nbsp;66270&nbsp;Le&nbsp;Soler</i></a></li>
-                      <li><a class="telephone"><i class="fa fa-phone">&nbsp;04&nbsp;68&nbsp;92&nbsp;52&nbsp;05</i></a></li>
-                      <li><a class="email"><i class="fa fa-envelope">&nbsp;brindebeautelesoler@gmail.com</i></a></li>
-                      <li class="" role="presentation"><a href="https://www.facebook.com/Le-Brin-de-Beaut%C3%A9-417084685132921/" class="facebook"><i class="fa fa-facebook" aria-hidden="true">&nbsp;Le&nbsp;Brin&nbsp;de&nbsp;Beauté</i></a></li>
-                    </ul>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11732.03133264411!2d2.7934539!3d42.6823764!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe3fa136135e70af9!2sInstitut+Le+Brin+de+Beaut%C3%A9!5e0!3m2!1sfr!2sfr!4v1526977004589"frameborder="0" allowfullscreen></iframe>
 
+    <div class="socialfooter">
+      <ul>
+        <li><a><i class="fa fa-map-marker">&nbsp;Place&nbsp;de&nbsp;la&nbsp;Rébuplique&nbsp;-&nbsp;66270&nbsp;Le&nbsp;Soler</i></a></li>
+        <li><a><i class="fa fa-phone">&nbsp;04&nbsp;68&nbsp;92&nbsp;52&nbsp;05</i></a></li>
+        <li><a><i class="fa fa-envelope">&nbsp;brindebeautelesoler@gmail.com</i></a></li>
+        <li role="presentation"><a href="https://www.facebook.com/Le-Brin-de-Beaut%C3%A9-417084685132921/" class="facebook"><i class="fa fa-facebook-official" aria-hidden="true">&nbsp;Le&nbsp;Brin&nbsp;de&nbsp;Beauté</i></a></li>
+      </ul>
+    </div>
 
-                    <!-- <button class="btn btn-primary" type="button" style="margin-top:20px;">Contact us</button> -->
-                </div>
-            </div>
-          </div>
-
-        </div>
-    </footer>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  </div>
+</footer>

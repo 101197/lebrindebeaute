275ef5be991dc0dffd29f1ee8e5a60362ce34034
101197 <valat.estelle@gmail.com>
2018-06-18 17:04:21 +0200

    debut accueil

--- accueil.php
+++ accueil.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <title>Le Brin de Beauté</title>
     <?php include 'assets/php/allcss.php'; ?>
+    <link href="https://fonts.googleapis.com/css?family=Tangerine" rel="stylesheet">
   </head>
   <body>
     <?php include 'assets/php/nav.php'; ?>
@@ -12,7 +13,15 @@
       <div class="bandeprincipale row mh-100vh">
         <div class="bg-white p-5 rounded my-4 my-lg-0" id="login-block">
 
+          <h6 style="text-align: center; font-family: 'Tangerine', cursive; font-size: 500%;">Le Brin de Beauté</h6>
 
+          <div class="ensembleaccueil">
+            <div class="part1accueil">
+              <img src="assets/img/vitrine.jpg" alt="vitrine" class="imgvitrine">
+              <p class="textvitrine">ouiouioui</p>
+
+            </div>
+          </div>
 
 
         </div>
